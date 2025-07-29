bc93d1017b012508b51403527a413d1bee81a44e
mitimugdho <bsse1648@iit.du.ac.bd>
2025-07-29 20:35:36 +0600

    attendance tracker

---
+++ src/Info.java
@@ -0,0 +1,19 @@
+public class Info {
+    private final int roll;
+    private final String name;
+    private int attendCount;
+    private int classCount=0;
+    private double percentage;
+
+    public Info(String name,int roll){
+        this.name=name;
+        this.roll=roll;
+        attendCount=0;
+    }
+    public void attendedClass(int period){
+        attendCount+=period;
+        classCount++;
+        percentage=attendCount/classCount*100.0;
+    }
+
+}
